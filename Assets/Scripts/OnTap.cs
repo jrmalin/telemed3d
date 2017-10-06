2661f9cd316e2cfa5a5b4944eec2fecfae06be3d
lengieza <lengieza@umich.edu>
2017-10-06 13:37:20 -0400

    added scripts to import file on tap

---
+++ Assets/Scripts/OnTap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class OnTap : MonoBehaviour {
+
+	public GameObject prefab;
+	public GameObject model;
+
+	[MenuItem("Example/Overwrite Texture")]
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver() {
+		if(Input.GetMouseButtonDown(0)){
+			StartCoroutine(OpenFilePicker());
+		}
+	}
+
+	IEnumerator OpenFilePicker() {
+		string path = EditorUtility.OpenFilePanel("Select a 3D model", "", "obj");
+		if (path.Length != 0) {
+			ObjImporter importer = new ObjImporter();
+			Mesh mesh = importer.ImportFile(path);
+			GameObject model = (GameObject)Instantiate(prefab);
+			model.GetComponent<MeshFilter>().mesh = mesh;
+			print ("made model");
+			yield return model;
+		}
+	}
+}
