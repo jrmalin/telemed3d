05671ec6435867f8dfbdcc0349a5999492b0d117
Justin Malin <jrmalin@umich.edu>
2017-11-02 18:04:59 -0400

    just need to add ML prediction, deep cleansing complete and parsed

--- pred_data/make_df.py
+++ pred_data/make_df.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 import warnings
 
 
@@ -31,6 +32,7 @@ def parse_features(filename):
       w = str(count) + ' items numbered "' + str(i) + '" instead of 1.'
       warnings.warn(w, Warning)
 
+  # Find data errors in features
   if len(features.isnull().any(1).nonzero()[0]) > 0:
     w = 'nan values at printed indices'
     print(features.isnull().any(1).nonzero())
@@ -40,19 +42,58 @@ def parse_features(filename):
   return features
 
 
-def main():
-  features = parse_features('tabula_desc.csv')
-  print(features.iloc[0:5])
+def create_row(features_loc, line):
+  new_row = []
+  for col_idx, input_idx in enumerate(features_loc):
+    if '-' in input_idx:
+      split = input_idx.split('-')
+      new_row.append(line[int(split[0]) - 1 : int(split[1])])
+    else:
+      new_row.append(line[int(input_idx) - 1])
 
+  return new_row
+
+
+def create_df(filename, features):
+  features_loc = features['FILE LOCATION']
+  features_name = features['[ITEM NAME], DESCRIPTION, AND CODES']
+  
+  f = open(filename, 'r')
+  lines = f.readlines()
+
+  data = []
+  for row_idx, line in enumerate(lines):
+    new_row = create_row(features_loc, line)
+    data.append(new_row)
+
+  df = pd.DataFrame(data, columns=features_name)
+  return df
+ 
+
+def main():
+  try:
+    print('loading namcs2015 dataframe...')
+    df = pd.read_pickle('namcs2015_parsed.pkl')
+    print('load complete')
+  except:
+    print('load failed')
+    print('creating namcs2015 dataframe...')
+    features = parse_features('tabula_desc.csv')
+    df = create_df('namcs2015', features)
+    print('creation complete')
+    print('saving namcs2015 dataframe...')
+    df.to_pickle('namcs2015_parsed.pkl')
+    print('save complete')
+
+  X = df[df.columns.difference(['[DIAG1R] DIAGNOSIS # 1 (Recode to Numeric Field)'])] 
+  y = df['[DIAG1R] DIAGNOSIS # 1 (Recode to Numeric Field)']
+#  clf = DecisionTreeClassifier()
+#  clf.fit(X, y)
+#  print(clf.score(X, y))
+  
 
 if __name__ == "__main__":
   main()
 
-
-# data = np.empty([1066, len(lines)])
-# print('shape:', data.shape)
-
-# f = open('namcs2015', 'r')
-# for line in f.readlines():
 
 
